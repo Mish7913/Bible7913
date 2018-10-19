cea8dd90ee55342bd49de5b04b7fb746acf7974e
Misha <mish7913@gmail.com>
2018-10-19 20:06:42 +0300

    Add files via upload

---
+++ releases/bible7913-v1.2-source-code/modules/modules.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  Copyright: 2017 Mish7913 <mish7913@gmail.com>
+#  License:   GNU General Public License v2 or later
+
+import orig;
+import ubio;
+import rst;
+import ru_strong;
+
+module = [orig, ubio, rst];
+dictionary = [];
+strong = [ru_strong];
